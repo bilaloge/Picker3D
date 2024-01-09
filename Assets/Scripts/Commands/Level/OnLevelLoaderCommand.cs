0565dacfd25b3b1153f94218f8c4201be45732e9
bilaloge <bilaloge01@gmail.com>
2024-01-09 19:20:47 +0300

    Input System Constuction beginning

--- Assets/Scripts/Commands/Level/OnLevelLoaderCommand.cs
+++ Assets/Scripts/Commands/Level/OnLevelLoaderCommand.cs
@@ -8,11 +8,11 @@ namespace Commands.Level
     {
         private Transform _levelHolder;
 
-        public OnLevelLoaderCommand(Transform levelHolder)
+        internal OnLevelLoaderCommand(Transform levelHolder)
         {
             _levelHolder = levelHolder;
         }
-        public void Execute(byte levelIndex)// son kýsým hariç prefab in clonunu oluþturmaya yarýyor
+        internal void Execute(byte levelIndex)// son kýsým hariç prefab in clonunu oluþturmaya yarýyor
         {
             /*var levelObject = */Object.Instantiate(original: Resources.Load<GameObject>(path: $"Prefabs/LevelPrefabs/Level {levelIndex}"), _levelHolder, worldPositionStays: true);
             //levelObject.transform.SetParent(_levelHolder); ya _levelholder,wordpos... yazýcaksýn, ya da yorum satýrýndakileri.
